b9eeeb2855df17b23e7f8bb8e964c4828b17a61b
MateusHBR <mateusmorais.r@gmail.com>
2021-07-01 17:10:10 -0300

    add musics to project

--- lib/infra/cache/cache_mock_adapter.dart
+++ lib/infra/cache/cache_mock_adapter.dart
@@ -3,6 +3,7 @@ import 'package:music_player/data/models/cache_music_model.dart';
 
 class CacheMockAdapter implements GetMusicsFromCacheStorage {
   final _imagesPath = "lib/ui/assets/images";
+  final _audiosPath = "lib/ui/assets/audios";
 
   @override
   Future<List<CacheMusicModel>> getMostPlayedCachedMusics() async {
@@ -12,25 +13,25 @@ class CacheMockAdapter implements GetMusicsFromCacheStorage {
         bandName: 'Paramore',
         musicName: "Ain't Fun",
         imagePath: '$_imagesPath/aint_fun.png',
-        musicPath: '/path/Paramore/music1',
+        musicPath: '',
       ),
       CacheMusicModel(
         bandName: 'Draft Punk',
         musicName: "Get Lucky",
         imagePath: '$_imagesPath/get_lucky.png',
-        musicPath: '/path/DraftPunk/music1',
+        musicPath: '',
       ),
       CacheMusicModel(
         bandName: 'Nothing But Thieves',
         musicName: "Impossible",
         imagePath: '$_imagesPath/impossible.png',
-        musicPath: '/path/Paramore/music1',
+        musicPath: '',
       ),
       CacheMusicModel(
         bandName: 'Depeche Mode',
         musicName: "Hole to Feed",
         imagePath: '$_imagesPath/hole_to_feed.png',
-        musicPath: '/path/DraftPunk/music1',
+        musicPath: '',
       ),
     ];
   }
@@ -43,13 +44,13 @@ class CacheMockAdapter implements GetMusicsFromCacheStorage {
         bandName: 'Foo Fighters',
         musicName: "Making A Fire",
         imagePath: '$_imagesPath/making_a_fire.png',
-        musicPath: '/path/Paramore/music1',
+        musicPath: '$_audiosPath/making_a_fire.mp3',
       ),
       CacheMusicModel(
-        bandName: 'WOW',
-        musicName: "Tiesto",
+        bandName: 'Tiesto',
+        musicName: "WOW",
         imagePath: '$_imagesPath/wow.png',
-        musicPath: '/path/DraftPunk/music1',
+        musicPath: '$_audiosPath/wow.mp3',
       ),
     ];
   }
